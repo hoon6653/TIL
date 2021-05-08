39639e815b5648d3789549f866f56e6c805d8f83
hoon6653 <theji@kakao.com>
2021-05-08 23:59:54 +0900

    0508

--- til.js
+++ til.js
@@ -29,3 +29,38 @@ class Stack {
         return result;
     }
 }
+
+
+//0508 큐 구현
+
+class Queue {
+    constructor() {
+        this.storage = {};
+        this.front = 0;
+        this.rear = 0;
+    }
+
+    size() {
+        return this.rear - this.front
+    }
+
+    // 큐에 데이터를 추가 할 수 있어야 함
+    enqueue(element) {
+        this.storage[this.rear] = element;
+        this.rear += 1;
+    }
+
+    // 가장 먼저 추가된 데이터가 가장 먼저 추출되어야함
+    dequeue() {
+        // 빈 큐에 dequeue 연산을 적용해도 에러가 발생하지 않아야 함
+        if (this.size() === 0) {
+            return;
+        }
+
+        const result = this.storage[this.front];
+        delete this.storage[this.front];
+        this.front += 1;
+
+        return result;
+    }
+}
