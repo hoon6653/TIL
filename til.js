aa4c7822c07222a4d1cc0498e9f5987c55d61f0f
hoon6653 <theji@kakao.com>
2021-05-11 21:55:04 +0900

    0511

--- til.js
+++ til.js
@@ -1,4 +1,4 @@
-//0507 스택구현
+//0508 스택구현
 class Stack {
     constructor() {
         this.storage = {};
@@ -31,7 +31,7 @@ class Stack {
 }
 
 
-//0508 큐 구현
+//0509 큐 구현
 
 class Queue {
     constructor() {
@@ -65,7 +65,7 @@ class Queue {
     }
 }
 
-//0509 브라우저 뒤로가기 앞으로가기
+//0510 브라우저 뒤로가기 앞으로가기
 function browserStack(actions, start) {
     let prevStack = [];
     let nextStack = [];
@@ -89,4 +89,47 @@ function browserStack(actions, start) {
         }
     }
     return [prevStack, current, nextStack]
+}
+
+// 0511 동화책 구현
+function improveBook(books, speeds) {
+    let answer = [];
+    let workDay = [];
+    let quotient = 0;
+    let remainder = 0;
+
+    // books 배열을 순회
+    for (let i = 0; i < books.length; i++) {
+        // 각 담당자마다 책이 발행되는 시간을 계산
+        quotient = Math.floor((100 - books[i]) / speeds[i]);
+        remainder = (100 - books[i]) % speeds[i];
+
+        // 만약 remainder가 0보다 크면 quotient에 1을 더함
+        if (remainder > 0) {
+            quotient = quotient + 1;
+        }
+
+        // workDay 배열에 차례대로 담기
+        // workDay 배열은 books배열에서 책이 발행되는 시간만을 추출해 넣은 배열
+        workDay.push(quotient);
+    }
+
+    // workDay 배열이 0보다 클 때까지 반복
+    while (workDay.length > 0) {
+
+        // workDay에서 0번째 인덱스보다 큰 크기의 인덱스를 찾아 deployIndex에 할당
+        let deployIndex = workDay.findIndex(fn => workDay[0] < fn);
+
+        if (deployIndex === -1) {
+            // 만약 찾지 못했다면 answer에 workDay 배열의 길이를 넣은 후, workDay 내부의 요소를 전부 삭제
+            answer.push(workDay.length);
+            workDay.splice(0, workDay.length);
+
+        } else {
+            // 만약 찾았다면, 해당 인덱스를 answer에 넣고, workDay에서 그만큼 제외
+            answer.push(deployIndex);
+            workDay.splice(0, deployIndex);
+        }
+    }
+    return answer;
 }
