a69a5f9d257ffbd341fd4f0e1bfa118d292edd66
hoon <theji@kakao.com>
2021-07-26 23:24:37 +0900

    toy4

--- til.js
+++ til.js
@@ -341,7 +341,9 @@ function getDirections(matrix, from, to) {
     }
 }
 
-// 0524 orderOfPresentation
+//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+
+// 0524 토이문제 1 orderOfPresentation
 function orderOfPresentation(N, K) {
     // 조의 개수 N, 발표 순서 K
 
@@ -408,7 +410,7 @@ function orderOfPresentation(N, K) {
     return order;
 }
 
-// 0525 fibonacci
+// 0525 토이문제 2 fibonacci
 let fibonacci = function (n) {
     const memo = [0, 1];
     const aux = (n) => {
@@ -421,7 +423,8 @@ let fibonacci = function (n) {
     return aux(n);
 };
 
-// 0526 isSubsetOf
+
+// 0526 토이문제 3 isSubsetOf
 const isSubsetOf = function (base, sample) {
     // naive solution: O(M * N)
     // return sample.every((item) => base.includes(item));
@@ -446,3 +449,68 @@ const isSubsetOf = function (base, sample) {
     }
     return true;
 };
+
+
+// 0726 토이문제 4 bubbleSort
+const swap = function (idx1, idx2, arr) {
+    // 두 변수를 바꾸는 방법
+
+    // 1) 임시 변수를 활용한 방법
+    // let temp = arr[idx1];
+    // arr[idx1] = arr[idx2];
+    // arr[idx2] = temp;
+
+    // 2) Destructuring assignment를 활용한 방법
+    // arr이 reference type이라 가능
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+
+    // 3) XOR 연산을 활용한 방법
+    // arr이 reference type이라 가능
+    // arr[idx1] ^= arr[idx2];
+    // arr[idx2] ^= arr[idx1];
+    // arr[idx1] ^= arr[idx2];
+};
+
+// naive solution
+// let bubbleSort = function (arr) {
+//   let N = arr.length;
+
+//   for (let i = 0; i < N - 1; i++) {
+//     // 매 반복(iteration)마다 i번째로 큰 수가 마지막에서 i번째 위치하게 된다.
+//     // 이미 정렬된 요소는 고려할 필요가 없으므로, 'j < N - 1 - i'만 비교하면 된다.
+//     for (let j = 0; j < N - 1 - i; j++) {
+//       if (arr[j] > arr[j + 1]) {
+//         swap(j, j + 1, arr);
+//       }
+//     }
+//   }
+
+//   return arr;
+// };
+
+// optimized solution
+let bubbleSort = function (arr) {
+    let N = arr.length;
+
+    for (let i = 0; i < N; i++) {
+        // swap 횟수를 기록한다.
+        // 어떤 요소도 swap되지 않은 경우, 배열은 정렬된 상태이다.
+        let swaps = 0;
+
+        // 매 반복(iteration)마다 i번째로 큰 수가 마지막에서 i번째 위치하게 된다.
+        // 이미 정렬된 요소는 고려할 필요가 없으므로, 'j < N - 1 - i'만 비교하면 된다.
+        for (let j = 0; j < N - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swaps++;
+                swap(j, j + 1, arr);
+            }
+        }
+
+        if (swaps === 0) {
+            break;
+        }
+    }
+
+    return arr;
+};
+
